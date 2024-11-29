fe67308bd927ec5c8b8c1b6b297680795273cdc5
Socrates Hlapolosa <socrates.hlapolosa@ghdlogistics.co.za>
2024-11-29 21:10:33 +0200

    refactor: Remove unused imports in ClientControllerTest

--- src/test/java/com/socrates/fin_app/identity/interfaces/ClientControllerTest.java
+++ src/test/java/com/socrates/fin_app/identity/interfaces/ClientControllerTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,7 +17,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.socrates.fin_app.functional.config.TestSecurityConfig;
 import com.socrates.fin_app.identity.application.dto.request.ClientRegistrationRequest;
 import com.socrates.fin_app.identity.application.dto.request.ForgotPasswordRequest;
 import com.socrates.fin_app.identity.application.dto.request.LoginRequest;
