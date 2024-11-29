6f642eb7b08c1c1dc97e0ecde584c0181ff63756
Socrates Hlapolosa <socrates.hlapolosa@ghdlogistics.co.za>
2024-11-29 12:00:16 +0200

    deleted

--- src/main/java/com/socrates/fin_app/identity/infrastructure/security/TokenProvider.java
+++
@@ -1,7 +0,0 @@
-package com.socrates.fin_app.identity.infrastructure.security;
-
-public interface TokenProvider {
-    String createToken(String username, String role);
-    String getUsername(String token);
-    String getRole(String token);
-}
